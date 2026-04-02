96f043cc8cb3c557b484c9e1741acba17ab384e7
Juan Camilo Realpe Ceron <juanrealpe972@gmail.com>
2026-04-02 18:28:55 -0500

    - Removed old frontend directory - Bootstrapped new Next.js app to update dependencies and environment variables - Clean slate for updated config and project structure

--- frontend/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center text-orange-500 py-4 mt-8">
-      &copy; 2026 Zira. Todos los derechos reservados.
-    </footer>
-  );
-}
